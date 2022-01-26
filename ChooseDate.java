2406eabe23e1af1425cf33b878aeb3c022093ef9
amirpabol <85292138+amirpabol@users.noreply.github.com>
2022-01-26 23:32:58 +0800

    Add files via upload

---
+++ ChooseDate.java
@@ -0,0 +1,13 @@
+package com.example.healthpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ FirstFragment.java
@@ -0,0 +1,32 @@
+package com.example.healthpoint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstFragment extends AppCompatActivity {
+
+    Button LoginBtn;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_first);
+
+        LoginBtn = findViewById(R.id.LOGIN);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SecondFragment.class));
+            }
+
+        });
+
+    }
+
+}
