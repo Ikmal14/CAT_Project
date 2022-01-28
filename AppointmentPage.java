c3e49ba1315c178832ed215bd51629e6e06f35f6
amirpabol <85292138+amirpabol@users.noreply.github.com>
2022-01-28 11:06:56 +0800

    Update AppointmentPage.java

--- AppointmentPage.java
+++ AppointmentPage.java
@@ -1,13 +1,47 @@
-package com.example.healthpoint;
+package com.example.healthpoint_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AppointmentPage extends AppCompatActivity {
+
+    Button setAppointment, myAppointment, infoButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_page);
+
+        setAppointment = findViewById(R.id.button_pending);
+        myAppointment = findViewById(R.id.button2);
+        infoButton = findViewById(R.id.Info);
+
+
+        setAppointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ChooseDate.class));
+            }
+        });
+
+        myAppointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MyAppointment.class ));
+            }
+        });
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.scrolling);
+            }
+        });
     }
 }
