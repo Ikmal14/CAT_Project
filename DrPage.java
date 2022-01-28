ab172ddea0f73924d8d709f1d1ff754f2e72e901
amirpabol <85292138+amirpabol@users.noreply.github.com>
2022-01-28 11:16:23 +0800

    Add files via upload

---
+++ DrPage.java
@@ -0,0 +1,14 @@
+package com.example.healthpoint_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dr_page);
+    }
+}
---
+++ MyAppointment.java
@@ -0,0 +1,24 @@
+package com.example.healthpoint_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MyAppointment extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.set_appointment_card);
+
+        }
+}
