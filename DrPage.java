d62371dd245dc9e286760f93fdb2c6c7e057192b
amirpabol <85292138+amirpabol@users.noreply.github.com>
2022-01-30 22:08:29 +0800

    Update DrPage.java

--- DrPage.java
+++ DrPage.java
@@ -1,14 +1,41 @@
 package com.example.healthpoint_final;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DrPage extends AppCompatActivity {
+
+    Button appointmentPg, DRfeedback, SubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dr_page);
+
+        appointmentPg = findViewById(R.id.viewAppointment);
+
+        appointmentPg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.dr_check_appointment);
+
+                DRfeedback = findViewById(R.id.feedbackDR);
+
+                DRfeedback.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        startActivity(new Intent(getApplicationContext(), feedback.class));
+                    }
+                });
+
+            }
+        });
+
     }
+
 }
