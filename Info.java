74eb318b229b94d462c60ddb349558d743e46483
Ikmal14 <91377763+Ikmal14@users.noreply.github.com>
2022-01-30 23:46:05 +0800

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,14 @@
+package com.example.HealthPointUSM;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
