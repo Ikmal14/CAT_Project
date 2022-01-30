38937eafebd502539c6e107c92e5f2a6e6992376
amirpabol <85292138+amirpabol@users.noreply.github.com>
2022-01-30 22:09:24 +0800

    Add files via upload

---
+++ FeedbackTEXT.java
@@ -0,0 +1,21 @@
+package com.example.healthpoint_final;
+
+public class FeedbackTEXT {
+
+    String feedback;
+
+    public FeedbackTEXT(){
+    }
+
+    public FeedbackTEXT(String feedback){
+        this.feedback = feedback;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
